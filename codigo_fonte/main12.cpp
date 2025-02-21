21493e9af197e5a8fb281282d365764825315e1e
lucagorayeb <lucagorayeb@gmail.com>
2025-02-21 16:09:31 -0400

    refazendo main 12

--- codigo_fonte/main12.cpp
+++ codigo_fonte/main12.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+class Analysis
+{
+    public:
+        Analysis (double grade){
+            setGrades(grade);
+        }
+
+        void setGrades(double grade){
+            int c=1;
+            while (c <= 10)
+            {
+                cout << "Enter grade:" << endl;
+                cin >> grade;
+                    if (grade>=8)
+                    {
+                        aproved=aproved+1;
+                    }else{
+                        reproved=reproved+1;
+                    }
+                c=c+1;
+            }
+            return getGrades();
+        }
+
+        void getGrades(){
+            cout << "Pass: " << aproved << endl;
+            cout << "Failure: " << reproved << endl;
+        }
+
+    private:
+        int reproved=0;
+        int aproved=0;
+};
+
 int main(){
-    cout << "Hello, world";
+
+    Analysis myAnalysis(0);
+    
 }
-
-
